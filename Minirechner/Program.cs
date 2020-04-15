23609e33b21a9732fff1ac9b3131d532380c0362
hannesa7x <bruhan@rgtfo-me.it>
2020-04-15 19:13:27 +0200

    Erstellung lokals VS-Projekt

---
+++ Minirechner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
